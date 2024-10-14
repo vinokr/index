bb846b46129a1a906c6909c54305c285125069cb
vinokr <vinokr0606@gmail.com>
2024-10-14 12:19:24 +0530

    Add files via upload

---
+++ Food delivery/README.md
@@ -0,0 +1,5 @@
+# food-delivery-website
+
+###### A simple food delivery website using HTML, CSS and JavaScript+
+
+See demo: https://fahadchy24.github.io/food-delivery-website/
---
+++ Food delivery/main.js
@@ -0,0 +1,45 @@
+//========================= Main Scripts ==================================//
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    // Navbar Behaviors
+    $('.navbar-toggle').on('click', function () {
+        $('.mobile-navbar').toggleClass('mobile-navbar-sidebar-open');
+        $('#toggle-icon').toggleClass('fa-times');
+    });
+
+    $(window).on('click', function () {
+        $('.mobile-navbar').removeClass('mobile-navbar-sidebar-open');
+        $('#toggle-icon').addClass('fas fa-bars');
+        $('#toggle-icon').removeClass('fa-times');
+    });
+
+    $('.navbar-toggle').on('click', function (event) {
+        event.stopPropagation();
+    });
+
+    $('.mobile-navbar').on('click', function (event) {
+        event.stopPropagation();
+    });
+});
